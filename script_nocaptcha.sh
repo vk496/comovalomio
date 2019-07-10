3d379ed3abbaae01a9f9d729e17356dc8c80adb6
Valentín Kivachuk <vk18496@gmail.com>
2019-07-10 22:03:21 +0200

    improove

--- script_nocaptcha.sh
+++ script_nocaptcha.sh
@@ -2,7 +2,7 @@
 set -e
 
 
-DEPS=( echo rm curl gm tesseract xmllint sed tr )
+DEPS=( echo rm curl gm xmllint sed )
 
 for i in "${DEPS[@]}"; do
 	if ! hash $i 2>/dev/null; then
@@ -10,6 +10,8 @@ for i in "${DEPS[@]}"; do
 		exit 1
 	fi
 done
+
+
 
 if [ $# -lt 4 ]; then
   echo "Número de parámetros incorrecto. Ejemplo:"
@@ -27,6 +29,7 @@ touch md5-$nie.sum
 
 if [[ "$orden" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]; then
 	emails="${@:4}"
+	orden= #reset
 else
 	emails="${@:5}"
 fi
@@ -34,7 +37,6 @@ fi
 send_email() {
 
   echo -e "$1" | ssmtp $emails
-  #echo -e "$emails"
   #echo -e "$1"
 }
 
@@ -48,39 +50,10 @@ curl --request GET \
     >/dev/null
 
 
-captcha=$(curl --request GET \
-  -s \
-  --url https://sede.mjusticia.gob.es/eConsultas/jcaptcha_image.action  \
-  --cookie-jar nada.txt \
-  --cookie nada.txt \
-  --output - | \
-gm convert \
-  - \
-  -fuzz 100% \
-  -opaque yellow \
-  -fill white \
-  - | gm convert - -fuzz 20% -fill white -opaque black - \
-| tesseract \
-  - \
-  stdout \
-  -l spa | \
-head -1)
-
-for i in "${PIPESTATUS[@]}"; do
-  if [[ $i -ne 0 ]]; then
-    send_email "Error críticio en procesar el captcha"
-    exit 1
-  fi
-done
-
-# echo $captcha
-#
-# set -x
-
 HTML=$(curl --request POST \
   -s \
   --url https://sede.mjusticia.gob.es/eConsultas/inicioNacionalidad  \
-  -d "formuNac.codigoNieCompleto=$nie&formuNac.numero=$num&formuNac.yearSolicitud=$year&formuNac.numOrden=$orden&jCaptchaResponse=$captcha&action:enviarDatosNacionalidad=Submit%2BQuery"  \
+  -d "formuNac.codigoNieCompleto=$nie&formuNac.numero=$num&formuNac.yearSolicitud=$year&formuNac.numOrden=$orden&action:enviarDatosNacionalidad=Submit%2BQuery"  \
   --cookie-jar nada.txt \
   --cookie nada.txt)
 
@@ -106,10 +79,10 @@ actual_md5=$(echo "$estado" | md5sum | awk '{print $1}')
 
 # echo $actual_md5
 
-if [[ ! $actual_md5 = $(cat md5-$nie.sum) ]]; then
-    echo "BINGO"
+if [ "$estado" ] && [[ ! $actual_md5 = $(cat md5-$nie.sum) ]]; then
+    logger "wait, BINGO"
     send_email "Subject: NACIONALIDAD\r\nContent-Type: text/html; charset=\"UTF-8\"\r\nAlgo ha cambiado... Revisa!.\nRaw Output:\n$estado"
     echo $actual_md5 > md5-$nie.sum
 else
-  echo "nop, nada..."
+  logger "wait. nop, nada..."
 fi
